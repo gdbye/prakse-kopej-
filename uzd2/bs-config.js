a5e182d8e098547ffd4b3325e7d12223ae2d8777
varbut <basens21@gmail.com>
2024-06-24 22:19:29 +0300

    ja

--- uzd2/bs-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    proxy: "localhost:3000",  // The Express server is running on this address
-    files: ["public/**/*.*"],  // Watch for changes in the "public" directory
-    port: 3001,                // BrowserSync server running on this port
-    open: false,               // Don't automatically open a browser window
-    notify: false              // Don't show BrowserSync notifications
-};
